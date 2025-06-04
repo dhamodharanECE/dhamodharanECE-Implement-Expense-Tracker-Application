e1324c314171bdb4db060d35f5935998e726038d
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-06-04 11:36:30 +0530

    add the Button function

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,22 @@
 
 import './App.css'
- import React from 'react'
+ import React, { useEffect, useState } from 'react'
  
  const App = () => {
+  const [data, setData] = useState(0);
+
+  function ClickButton(){
+    alert(`Count:${data}`);
+    setData(data + 1)
+  }
+  console.log(data);
+
    return (
-     <div>App</div>
+    <>
+     <div>
+       <button onClick={ClickButton} style={{ backgroundColor: 'blue', width: '100px', height: '50px', border:'5px solid gold', borderRadius: '10px' }}>BUTTON</button>
+     </div>
+    </>
    )
  }
  
