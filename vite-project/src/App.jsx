d528c10ab0501aa5031d3a01cd7788c86944e9b0
DHAMODHARAN S <dhamodharansece23@gmail.com>
2025-06-06 16:39:23 +0530

    Add in Chart digram and functionalty

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
 import React, { useState } from 'react'
-
 import { FirstContext, SecondContext } from './Context';
 import Header from './Header/Header';
 import { Datasheet } from './Datasheet';
+import TranscationsChart from './TranscationsChart/TranscationsChart';
+
 
 
 const App = () => {
@@ -12,11 +13,20 @@ const App = () => {
       expense: 1200
   });
   const [transactionData, setTransactionData] = useState(Datasheet);
+
+  const value = JSON.stringify(money);
+  const data = JSON.stringify(transactionData);
+  localStorage.setItem("Add Income", value);
+  localStorage.setItem("Expense data", data);
+  sessionStorage.setItem("Add data", value);
+  sessionStorage.setItem("Expense data", data);
+
   return (
   <main>
    <FirstContext.Provider value={[money, setMoney]}>
-    <SecondContext.Provider transactionData={transactionData}>
-       <Header balance={money.balance} expense={money.expense} transactionData={transactionData}/>
+    <SecondContext.Provider value={transactionData}>
+       <Header balance={money.balance} expense={money.expense}/>
+       <TranscationsChart transactionData={transactionData}/>
     </SecondContext.Provider>
    </FirstContext.Provider>
     
